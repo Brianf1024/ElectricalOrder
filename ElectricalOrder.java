af7f7906b1e9b700417f3bea0e15fceeb9a9d7b6
Brianf1024 <165444892+Brianf1024@users.noreply.github.com>
2024-09-10 10:51:52 -0700

    Update ElectricalOrder.java

--- ElectricalOrder.java
+++ ElectricalOrder.java
@@ -18,10 +18,10 @@ public class ElectricalOrder {
 		double totalAmount, totalTax, totalWithTax;
 		
 		// Gets customer name 
-		//System.out.print("Enter customer name:");
+		System.out.print("Enter customer name:");
 		// System.in to read user input
-		//Scanner input = new Scanner(System.in);
-		//customerName = input.nextLine(); 
+		Scanner input = new Scanner(System.in);
+		customerName = input.nextLine(); 
 		
 		// Dialog boxes to read quantity of each item from the order form
 		item1Quant = Integer.parseInt(JOptionPane.showInputDialog("Enter the quantity for 120V 500 Watts (5A) Motor & Equipment Outlets for $144.82 each:"));
@@ -41,14 +41,14 @@ public class ElectricalOrder {
 		int hundredDigit = (int) totalWithTax/ 100 % 10;
 		
 		// Formatted output data using System.out.printf
-		  //System.out.printf("Customer Name: %s\n", customerName);
+	      System.out.printf("Customer Name: %s\n", customerName);
 	      System.out.printf("Total Amount Before Tax: $%.2f\n", totalAmount);
 	      System.out.printf("Tax: $%.2f\n", totalTax);
 	      System.out.printf("Total with Tax: $%.2f\n", totalWithTax);
 	      System.out.printf("Hundreds Digit of the Total: %d\n", hundredDigit);
 	
-	    //input.close();
-		System.exit(0);
+	    input.close();
+	    System.exit(0);
 		
 	}
 
